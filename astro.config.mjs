9b8194932fb3d6e9285a0534d2af0c99b1da3fb2
y0x3 <diegocmunoz@hotmail.com>
2025-04-21 09:22:46 -0600

    Movidos los archivos CSS a public/styles y actualizadas las rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://pag-eve-v2.vercel.app/', // Cambia esto por tu dominio en Vercel
 });
